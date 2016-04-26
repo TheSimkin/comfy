7ab603109859df878d827176c5f7d5169e2d9989
Stephen DeGabriele <stephen@degabriele.com>
2016-04-26 15:43:55 -0700

    add me to docker group

--- ubuntuBaseInit.sh
+++ ubuntuBaseInit.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install git
 sudo apt-get -y install docker.io
+sudo usermod -aG docker sdegabri
 sudo apt-get -y install ruby
 sudo apt-get -y install fish
 sudo apt-get -y install emacs
